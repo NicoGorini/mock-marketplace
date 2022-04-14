d0110dc91dc8a0185ec9a677c0bcdd83325bbda1
NicoGorini <gorininicolas12@gmail.com>
2022-04-14 16:48:59 +0200

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -5,33 +5,40 @@ import Link from 'next/link'
 function MyApp({ Component, pageProps }) {
   return (
     <div>
-      <nav className="border-b p-6">
-        <p className="text-4xl font-bold">Metaverse Marketplace</p>
-        <div className="flex mt-4">
-          <Link href="/">
-            <a className="mr-4 text-pink-500">
-              Home
-            </a>
-          </Link>
-          <Link href="/create-nft">
-            <a className="mr-6 text-pink-500">
-              Sell NFT
-            </a>
-          </Link>
-          <Link href="/my-nfts">
-            <a className="mr-6 text-pink-500">
-              My NFTs
-            </a>
-          </Link>
-          <Link href="/dashboard">
-            <a className="mr-6 text-pink-500">
-              Dashboard
-            </a>
-          </Link>
+    <nav className="bg-white border-gray-200 px-2 sm:px-6 py-5 dark:bg-gray-800">
+      <div className="container flex flex-wrap justify-between items-center mx-auto">
+        <a className="flex items-center">
+            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Fitstic Marketplace</span>
+        </a>
+      
+        <div class="hidden w-full md:block md:w-auto" id="mobile-menu">
+          <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+            <Link href="/">
+                <a className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                  Home
+                </a>
+              </Link>
+              <Link href="/create-nft">
+                <a className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                  Vendi il tuo Nft 
+                </a>
+              </Link>
+              <Link href="/my-nfts">
+                <a className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                  I tuoi Nft
+                </a>
+              </Link>
+              <Link href="/dashboard">
+                <a className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                  Dashboard
+                </a>
+              </Link>
+            </ul>
         </div>
-      </nav>
-      <Component {...pageProps} />
-    </div>
+      </div>
+    </nav>
+    <Component {...pageProps} />
+  </div>
   )
 }
 
--- pages/create-nft.js
+++ pages/create-nft.js
@@ -73,17 +73,17 @@ export default function CreateItem() {
     <div className="flex justify-center">
       <div className="w-1/2 flex flex-col pb-12">
         <input 
-          placeholder="Asset Name"
+          placeholder="Nome dell'NFT"
           className="mt-8 border rounded p-4"
           onChange={e => updateFormInput({ ...formInput, name: e.target.value })}
         />
         <textarea
-          placeholder="Asset Description"
+          placeholder="Descrizione"
           className="mt-2 border rounded p-4"
           onChange={e => updateFormInput({ ...formInput, description: e.target.value })}
         />
         <input
-          placeholder="Asset Price in Eth"
+          placeholder="Inserisci il prezzo in ETH (Ethereum)"
           className="mt-2 border rounded p-4"
           onChange={e => updateFormInput({ ...formInput, price: e.target.value })}
         />
@@ -99,7 +99,7 @@ export default function CreateItem() {
           )
         }
         <button onClick={listNFTForSale} className="font-bold mt-4 bg-pink-500 text-white rounded p-4 shadow-lg">
-          Create NFT
+          Vendi il tuo Nft
         </button>
       </div>
     </div>
